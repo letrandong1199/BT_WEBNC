09d7ac020aa65bb184524d6d76b4c8469f24c119
le tran dong <letrandong1199@gmail.com>
2021-04-06 00:39:19 +0700

    ADD detail of actor

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
+const cors = require('cors')
+
+
+app.use(cors());
+
+
 const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.json());
@@ -12,6 +19,22 @@ app.use('/api/countries', require('./routes/country.route'));
 
 app.get('/', (req, res) => {
     res.send("Hello Sakila...........")
+})
+
+app.get('/err', function (req, res) {
+    throw new Error('Error!!!');
+})
+app.use(function (req, res, next) {
+    res.status(404).json({
+        error_message: "Endpoint not found"
+    });
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).json({
+        error_message: 'Something broke !!!!!'
+    })
 })
 const PORT = 3000;
 app.listen(PORT, () => {
